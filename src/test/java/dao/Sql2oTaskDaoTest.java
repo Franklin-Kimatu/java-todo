5b8a37d1740fd018ed7dbd51e349d6e6d136d88b
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-06 18:08:04 +0300

    added some functionality

---
+++ src/test/java/dao/Sql2oTaskDaoTest.java
@@ -0,0 +1,83 @@
+package dao;
+
+import models.Task;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Sql2oTaskDaoTest {
+    private Sql2oTaskDao taskDao; //ignore me for now. We'll create this soon.
+    private Connection conn; //must be sql2o class conn
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        taskDao = new Sql2oTaskDao(sql2o); //ignore me for now
+        conn = sql2o.open(); //keep connection open through entire test so it does not get erased
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void addingCourseSetsId() throws Exception{
+        Task task = new Task ("mow the lawn");
+        int originalTaskId = task.getId();
+        taskDao.add(task);
+        assertNotEquals(originalTaskId, task.getId());
+
+    }
+    @Test
+    public void existingTasksCanBeFoundById() throws Exception {
+        Task task = new Task ("mow the lawn");
+        taskDao.add(task); //add to dao (takes care of saving)
+        Task foundTask = taskDao.findById(task.getId()); //retrieve
+        assertEquals(task, foundTask); //should be the same
+    }
+    // Test for finding tasks by id
+    @Test
+    public void allTasksAreFoundAndReturned() throws Exception{
+        Task task = new Task("mow the lawn");
+        taskDao.add(task);
+        assertEquals(1,taskDao.getAll().size());
+
+    }
+
+    @Test
+    public void noTasksReturnsEmptyList() throws Exception{
+        assertEquals(0,taskDao.getAll().size());
+    }
+
+    @Test
+    public void updatingPosts() throws Exception{
+     String initialDescription = "mow the lawn";
+     Task task = new Task(initialDescription);
+     taskDao.add(task);
+     taskDao.update(task.getId(),"brush the cat");
+     Task updatedTask = taskDao.findById(task.getId());
+     assertNotEquals(initialDescription,updatedTask.getDescription());
+    }
+    // deleting post by Id
+
+    @Test
+    public void deletingPostsByCorrectId() throws Exception{
+        Task task = new Task ("mow the lawn");
+        taskDao.add(task);
+        taskDao.deleteById(task.getId());
+        assertEquals(0,taskDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAllPosts() throws Exception{
+        Task task= new Task ("mow the lawn");
+        Task otherTask = new Task ("brush the cat");
+        taskDao.add(task);
+        taskDao.add(otherTask);
+        int daoSize = taskDao.getAll().size();
+        taskDao.clearAllTasks();
+        assertTrue(daoSize>0 && daoSize>taskDao.getAll().size());
+    }
+}
